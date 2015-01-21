0cb978a589420c6d76c914e597beed1d36a713de
09kellyc <chrisjpkelly@gmail.com>
2015-01-21 21:09:10 +0000

    hh10d sensor
    Using the frequency counter library, and an equation humidity can be
    calculated.

---
+++ humidity.ino
@@ -0,0 +1,16 @@
+#include <FreqCounter.h>
+long int frq;
+void setup() {
+  Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
+}
+void loop() {
+  FreqCounter::f_comp= 8;             // Set compensation to 12
+  FreqCounter::start(100);            // Start counting with gatetime of 100ms
+  while (FreqCounter::f_ready == 0)         // wait until counter ready
+
+    frq=FreqCounter::f_freq;            // read result  
+  //calculation for relative humidity
+  double humidity = ((7790-(frq*10))*393/4096)+16;
+  Serial.println (humidity);
+}
+
